b1b6bb846359095a9042cbc8b7e67b56ab2620fe
Alexander Sashnov <sashnov@ngs.ru>
2005-09-12 09:21:11 +0700

    correct ayemu_vtx_sprintname(); increase checking for errors

--- src/vtxfile.c
+++ src/vtxfile.c
@@ -1,5 +1,6 @@
 #include <string.h>
 #include <stdlib.h>
+#include <errno.h>
 #include <assert.h>
 
 #include "ayemu.h"
@@ -7,34 +8,90 @@
 /* defined in lh5dec.c */
 extern void lh5_decode(unsigned char *inp,unsigned char *outp,unsigned long original_size, unsigned long packed_size);
 
-/* read 16-bit integer from file. Return 1 if success */
+
+/* Read 8-bit integer from file.
+ * Return 1 if error occurs
+ */
+static int read_byte(FILE *fp, int *p)
+{
+	int c;
+	if ((c = getc(fp)) == EOF) {
+		perror("libayemu: read_byte()");
+		return 1;
+	}
+	*p = c;
+	return 0;
+}
+
+/* Read 16-bit integer from file.
+ * Return 1 if error occurs
+ */
 static int read_word16(FILE *fp, int *p)
 {
-  int c;
-  *p = getc(fp);
-  *p += ((c = getc(fp)) << 8);
-  return (c == EOF)? 0 : 1;
+	int c;
+	if ((c = getc(fp)) == EOF) {
+		perror("libayemu: read_word16()");
+		return 1;
+	}
+	*p = c;
+
+	if ((c = getc(fp)) == EOF) {
+		perror("libayemu: read_word16()");
+		return 1;
+	}
+	*p += c << 8;
+
+	return 0;
 }
 
-/* read 32-bit integer from file. Returns 1 if success */
+/* read 32-bit integer from file.
+ *  Returns 1 if error occurs 
+ */
 static int read_word32(FILE *fp, int *p)
 {
-  int c;
-  *p = fgetc(fp);
-  *p += (getc(fp) << 8);
-  *p += (getc(fp) << 16);
-  *p += ((c = getc(fp)) << 24);
-  return (c == EOF)? 0 : 1;
+	int c;
+
+	if ((c = getc(fp)) == EOF) {
+		perror("libayemu: read_word32()");
+		return 1;
+	}
+	*p = c;
+
+	if ((c = getc(fp)) == EOF) {
+		perror("libayemu: read_word32()");
+		return 1;
+	}
+	*p += c << 8;
+
+	if ((c = getc(fp)) == EOF) {
+		perror("libayemu: read_word32()");
+		return 1;
+	}
+	*p += c << 16;
+
+	if ((c = getc(fp)) == EOF) {
+		perror("libayemu: read_word32()");
+		return 1;
+	}
+	*p += c << 24;
+
+	return 0;
 }
 
-/* read_NTstring: reads null-terminated string from file. Return strlen. */
+/* read_NTstring: reads null-terminated string from file.
+ *  Return 1 if error occures.
+ */
 static int read_NTstring(FILE *fp, char s[])
 {
-  int i, c;
-  for (i = 0 ; i < AYEMU_VTX_NTSTRING_MAX && (c = getc(fp)) != EOF && c ; i++)
-    s[i] = c;
-  s[i] = '\0';
-  return i;
+	int i, c;
+	for (i = 0 ; i < AYEMU_VTX_NTSTRING_MAX && (c = getc(fp)) != EOF && c ; i++)
+		s[i] = c;
+	s[i] = '\0';
+	if (c == EOF) {
+		fprintf(stderr, "libayemu: read_NTstring(): uninspected end of file!\n");
+		return 1;
+	}
+	return 0;
 }
 
 /** Open specified .vtx file and read vtx file header
@@ -44,41 +101,51 @@ static int read_NTstring(FILE *fp, char s[])
  */
 int ayemu_vtx_open (ayemu_vtx_t *vtx, const char *filename)
 {
-  char buf[2];
-  vtx->regdata = NULL;
-  if ((vtx->fp = fopen (filename, "rb")) == NULL) {
-    fprintf(stderr, "ayemu_vtx_open: Cannot open file %s\n", filename);
-    return 0;
-  }
-  if (fread(buf, 2, 1, vtx->fp) != 2) {
-    fprintf(stderr,"ayemu_vtx_open: Can't read from %s\n", filename);
-    return 0;
-  }
-  buf[0] = tolower(buf[0]);
-  buf[1] = tolower(buf[1]);
-  if (strncmp(buf, "ay", 2) == 0)
-    vtx->hdr.chiptype = AYEMU_AY;
-  else if (strncmp (buf, "ym", 2) == 0)
-    vtx->hdr.chiptype = AYEMU_YM;
-  else {
-    fprintf (stderr, "File %s is _not_ VORTEX format!\nIt not begins from AY or YM.\n", filename);
-    fclose (vtx->fp);
-    vtx->fp = NULL;
-    return 0;
-  }
-  /* read VTX header info in order format specified, see http:// ..... */
-  vtx->hdr.stereo = fgetc (vtx->fp);
-  read_word16(vtx->fp, &vtx->hdr.loop);
-  read_word32(vtx->fp, &vtx->hdr.chipFreq);
-  vtx->hdr.playerFreq = fgetc (vtx->fp);
-  read_word16(vtx->fp, &vtx->hdr.year);
-  read_word32(vtx->fp, &vtx->hdr.regdata_size);
-  read_NTstring(vtx->fp, vtx->hdr.title);
-  read_NTstring(vtx->fp, vtx->hdr.author);
-  read_NTstring(vtx->fp, vtx->hdr.from);
-  read_NTstring(vtx->fp, vtx->hdr.tracker);
-  /* if error occurs on read last line return false */
-  return read_NTstring (vtx->fp, vtx->hdr.comment);
+	char buf[2];
+	int error = 0;
+
+	vtx->regdata = NULL;
+
+	if ((vtx->fp = fopen (filename, "rb")) == NULL) {
+		fprintf(stderr, "ayemu_vtx_open: Cannot open file %s: %s\n", filename, strerror(errno));
+		return 0;
+	}
+
+	if (fread(buf, 2, 1, vtx->fp) != 1) {
+		fprintf(stderr,"ayemu_vtx_open: Can't read from %s: %s\n", filename, strerror(errno));
+		error = 1;
+	}
+
+	buf[0] = tolower(buf[0]);
+	buf[1] = tolower(buf[1]);
+
+	if (strncmp(buf, "ay", 2) == 0)
+		vtx->hdr.chiptype = AYEMU_AY;
+	else if (strncmp (buf, "ym", 2) == 0)
+		vtx->hdr.chiptype = AYEMU_YM;
+	else {
+		fprintf (stderr, "File %s is _not_ VORTEX format!\nIt not begins from AY or YM.\n", filename);
+		error = 1;
+	}
+
+	/* read VTX header info in order format specified, see http:// ..... */
+	if (!error) error = read_byte(vtx->fp, &vtx->hdr.stereo);
+	if (!error) error = read_word16(vtx->fp, &vtx->hdr.loop);
+	if (!error) error = read_word32(vtx->fp, &vtx->hdr.chipFreq);
+	if (!error) error = read_byte(vtx->fp, &vtx->hdr.playerFreq);
+	if (!error) error = read_word16(vtx->fp, &vtx->hdr.year);
+	if (!error) error = read_word32(vtx->fp, &vtx->hdr.regdata_size);
+	if (!error) error = read_NTstring(vtx->fp, vtx->hdr.title);
+	if (!error) error = read_NTstring(vtx->fp, vtx->hdr.author);
+	if (!error) error = read_NTstring(vtx->fp, vtx->hdr.from);
+	if (!error) error = read_NTstring(vtx->fp, vtx->hdr.tracker);
+	if (!error) error = read_NTstring (vtx->fp, vtx->hdr.comment);
+
+	if (error) {
+		fclose(vtx->fp);
+		vtx->fp = NULL;
+	}
+	return !error;
 }
 
 /** Read and encode lha data from .vtx file
@@ -88,42 +155,42 @@ int ayemu_vtx_open (ayemu_vtx_t *vtx, const char *filename)
  */
 char *ayemu_vtx_load_data (ayemu_vtx_t *vtx)
 {
-  char *packed_data;
-  size_t packed_size;
-  size_t buf_alloc;
-  int c;
+	char *packed_data;
+	size_t packed_size;
+	size_t buf_alloc;
+	int c;
 
-  if (vtx->fp == NULL) {
-    fprintf(stderr, "ayemu_vtx_load_data: tune file not open yet (do you call ayemu_vtx_open first?)\n");
-    return NULL;
-  }
-  packed_size = 0; 
-  buf_alloc = 4096;
-  packed_data = (char *) malloc (buf_alloc);
-  /* read packed AY register data to end of file. */
-  while ((c = fgetc (vtx->fp)) != EOF) {
-    if (buf_alloc < packed_size) {              
-      buf_alloc *= 2;
-      packed_data = (char *) realloc (packed_data, buf_alloc);
-      if (packed_data == NULL) {
-	fprintf (stderr, "ayemu_vtx_load_data: Packed data out of memory!\n");
+	if (vtx->fp == NULL) {
+		fprintf(stderr, "ayemu_vtx_load_data: tune file not open yet (do you call ayemu_vtx_open first?)\n");
+		return NULL;
+	}
+	packed_size = 0; 
+	buf_alloc = 4096;
+	packed_data = (char *) malloc (buf_alloc);
+	/* read packed AY register data to end of file. */
+	while ((c = fgetc (vtx->fp)) != EOF) {
+		if (buf_alloc < packed_size) {              
+			buf_alloc *= 2;
+			packed_data = (char *) realloc (packed_data, buf_alloc);
+			if (packed_data == NULL) {
+				fprintf (stderr, "ayemu_vtx_load_data: Packed data out of memory!\n");
+				fclose (vtx->fp);
+				return NULL;
+			}
+		}
+		packed_data[packed_size++] = c;
+	}  
 	fclose (vtx->fp);
-	return NULL;
-      }
-    }
-    packed_data[packed_size++] = c;
-  }  
-  fclose (vtx->fp);
-  vtx->fp = NULL;
-  if ((vtx->regdata = (char *) malloc (vtx->hdr.regdata_size)) == NULL) {
-    fprintf (stderr, "ayemu_vtx_load_data: Can allocate %d bytes for unpack register data\n", vtx->hdr.regdata_size);
-    free (packed_data);
-    return NULL;
-  }
-  lh5_decode (packed_data, vtx->regdata, vtx->hdr.regdata_size, packed_size);
-  free (packed_data);
-  vtx->pos = 0;
-  return vtx->regdata;
+	vtx->fp = NULL;
+	if ((vtx->regdata = (char *) malloc (vtx->hdr.regdata_size)) == NULL) {
+		fprintf (stderr, "ayemu_vtx_load_data: Can allocate %d bytes for unpack register data\n", vtx->hdr.regdata_size);
+		free (packed_data);
+		return NULL;
+	}
+	lh5_decode (packed_data, vtx->regdata, vtx->hdr.regdata_size, packed_size);
+	free (packed_data);
+	vtx->pos = 0;
+	return vtx->regdata;
 }
 
 /** Get next 14-bytes frame of AY register data.
@@ -132,16 +199,37 @@ char *ayemu_vtx_load_data (ayemu_vtx_t *vtx)
  */
 int ayemu_vtx_get_next_frame (ayemu_vtx_t *vtx, char *regs)
 {
-  int numframes = vtx->hdr.regdata_size / 14;
-  if (vtx->pos++ >= numframes)
-    return 0;
-  else {
-    int n;
-    char *p = vtx->regdata + vtx->pos;
-    for(n = 0 ; n < 14 ; n++, p+=numframes)
-      regs[n] = *p;
-    return 1;
-  }
+	int numframes = vtx->hdr.regdata_size / 14;
+	if (vtx->pos++ >= numframes)
+		return 0;
+	else {
+		int n;
+		char *p = vtx->regdata + vtx->pos;
+		for(n = 0 ; n < 14 ; n++, p+=numframes)
+			regs[n] = *p;
+		return 1;
+	}
+}
+
+static void append_string(char *buf, const int sz, const char *str)
+{
+	if (strlen(buf) + strlen(str) < sz - 1)
+		strcat(buf, str);
+}
+
+static void append_number(char *buf, const int sz, const int num)
+{
+	char s[32];
+	snprintf(s, sizeof(s), "%d", num);
+	return append_string(buf, sz, s);
+}
+
+static void append_char(char *buf, const int sz, const char c)
+{
+	int pos = strlen(buf);
+	if (pos < sz - 1)
+		buf[pos++] = c;
+	buf[pos] = '\0';
 }
 
 /** Print formated file name. If fmt is NULL the default format %a - %t will used
@@ -159,66 +247,59 @@ int ayemu_vtx_get_next_frame (ayemu_vtx_t *vtx, char *regs)
  * %F chip Freq
  * %P player freq
  */
-void ayemu_vtx_sprintname (ayemu_vtx_t *vtx, char *buf, int bufsize, char *fmt)
+void ayemu_vtx_sprintname (const ayemu_vtx_t *vtx, char *const buf, const int sz, const char *fmt)
 {
-#define FMT_STRING s
-#define FMT_NUM d
-#define APPEND(type, what) \
-  do {\
-  int n;\
-  n = sprintf(buf, "%*" #type, bufsize, what);\
-  bufsize -= n;\
-  buf += n;\
-  } while (0)
+	static char *stereo_types[] = { "MONO", "ABC", "ACB", "BAC", "BCA", "CAB", "CBA" };
 
-  char *stereo_types[] = { "MONO", "ABC", "ACB", "BAC", "BCA", "CAB", "CBA" };
+	if (fmt == NULL)
+		fmt = "%a - %t";
 
-  if (fmt == NULL)
-    fmt = "%a - %t";
-  
-  while (--bufsize > 0 && *fmt != '\0')
-	  if (*fmt != '%')
-		  *buf++ = *fmt++;
-	  else {
-		  char c = *++fmt;
-		  switch(c) {
-		  case 'a':
-			  APPEND(FMT_STRING, vtx->hdr.author);
-			  break;
-		  case 't':
-			  APPEND(FMT_STRING, vtx->hdr.title);
-			  break;
-		  case 'y':
-			  APPEND(FMT_NUM, vtx->hdr.year);
-			  break;
-		  case 'f':
-			  APPEND(FMT_STRING, vtx->hdr.from);
-			  break;
-		  case 'T':
-			  APPEND(FMT_STRING, vtx->hdr.tracker);
-			  break;
-		  case 'C':
-			  APPEND(FMT_STRING, vtx->hdr.comment);
-			  break;
-		  case 's':
-			  APPEND(FMT_STRING, stereo_types[vtx->hdr.stereo]);
-			  break;
-		  case 'l':
-			  APPEND(FMT_STRING, (vtx->hdr.loop)? "looped" : "non-looped" );
-			  break;
-		  case 'c':
-			  APPEND(FMT_STRING, (vtx->hdr.chiptype == AYEMU_AY)? "AY" : "YM" );
-			  break;
-		  case 'F':
-			  APPEND(FMT_NUM, vtx->hdr.chipFreq);
-			  break;
-		  case 'P':
-			  APPEND(FMT_NUM, vtx->hdr.playerFreq);
-			  break;
-		  default:
-			  *buf++ = c;
-		  }
-	  }
+	buf[0] = '\0';
+
+	while (*fmt != '\0') {
+		if (*fmt == '%') {
+			switch(*++fmt) {
+			case 'a':
+				append_string(buf, sz, vtx->hdr.author);
+				break;
+			case 't':
+				append_string(buf, sz, vtx->hdr.title);
+				break;
+			case 'y':
+				append_number(buf, sz, vtx->hdr.year);
+				break;
+			case 'f':
+				append_string(buf, sz, vtx->hdr.from);
+				break;
+			case 'T':
+				append_string(buf, sz, vtx->hdr.tracker);
+				break;
+			case 'C':
+				append_string(buf, sz, vtx->hdr.comment);
+				break;
+			case 's':
+				append_string(buf, sz, stereo_types[vtx->hdr.stereo]);
+				break;
+			case 'l':
+				append_string(buf, sz, (vtx->hdr.loop)? "looped" : "non-looped" );
+				break;
+			case 'c':
+				append_string(buf, sz, (vtx->hdr.chiptype == AYEMU_AY)? "AY" : "YM" );
+				break;
+			case 'F':
+				append_number(buf, sz, vtx->hdr.chipFreq);
+				break;
+			case 'P':
+				append_number(buf, sz, vtx->hdr.playerFreq);
+				break;
+			default:
+				append_char(buf, sz, *fmt);
+			}
+			fmt++;
+		} else {
+			append_char(buf, sz, *fmt++);
+		}
+	}
 }
 
 /** Free all of allocaded resource for this file.
@@ -227,12 +308,13 @@ void ayemu_vtx_sprintname (ayemu_vtx_t *vtx, char *buf, int bufsize, char *fmt)
  */
 void ayemu_vtx_free (ayemu_vtx_t *vtx)
 {
-  if (vtx->fp) {
-    fclose (vtx->fp);
-    vtx->fp = NULL;
-  }
-  if (vtx->regdata) {
-    free (vtx->regdata);
-    vtx->regdata = NULL;
-  }
+	if (vtx->fp) {
+		fclose(vtx->fp);
+		vtx->fp = NULL;
+	}
+
+	if (vtx->regdata) {
+		free(vtx->regdata);
+		vtx->regdata = NULL;
+	}
 }
