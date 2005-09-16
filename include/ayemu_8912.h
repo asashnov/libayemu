0d9cb9b432dfb7bfdcf8b1738a2f69b450680819
Alexander Sashnov <sashnov@ngs.ru>
2005-09-16 08:53:14 +0700

    document all structures members in doxygen format

--- include/ayemu_8912.h
+++ include/ayemu_8912.h
@@ -1,3 +1,7 @@
+/**
+ * AY/YM emulator include file
+ */
+
 #ifndef _AYEMU_ay8912_h
 #define _AYEMU_ay8912_h
 
@@ -6,7 +10,7 @@
 BEGIN_C_DECLS
 
 /** Types of stereo.
-    The stereo types codes used for generage sound. */
+    The codes of stereo types used for generage sound. */
 typedef enum
 {
   AYEMU_MONO = 0,
@@ -19,85 +23,101 @@ typedef enum
   AYEMU_STEREO_CUSTOM = 255
 } ayemu_stereo_t;
 
-/** Sound chip type */
+/** Sound chip type.
+    Constant for identify used chip for emulation */
 typedef enum {
-  AYEMU_AY,
-  AYEMU_YM,
-  AYEMU_AY_LION17,
-  AYEMU_YM_LION17,
-  AYEMU_AY_KAY,
-  AYEMU_YM_KAY,
-  AYEMU_AY_CUSTOM,
-  AYEMU_YM_CUSTOM
+  AYEMU_AY,			/**< default AY chip (lion17 for now) */
+  AYEMU_YM,			/**< default YM chip (lion17 for now) */
+  AYEMU_AY_LION17,		/**< emulate AY with Lion17 table */
+  AYEMU_YM_LION17,		/**< emulate YM with Lion17 table */
+  AYEMU_AY_KAY,			/**< emulate AY with HACKER KAY table */
+  AYEMU_YM_KAY,			/**< emulate YM with HACKER KAY table */
+  AYEMU_AY_LOG,			/**< emulate AY with logariphmic table */
+  AYEMU_YM_LOG,			/**< emulate YM with logariphmic table */
+  AYEMU_AY_CUSTOM,		/**< use AY with custom table. */
+  AYEMU_YM_CUSTOM		/**< use YM with custom table. */
 } ayemu_chip_t;
 
-/** AY registers data */
+/** parsed by #ayemu_set_regs() AY registers data \internal */
 typedef struct
 {
-  int tone_a;           /** R0, R1 */
-  int tone_b;		/** R2, R3 */	
-  int tone_c;		/** R4, R5 */
-  int noise;		/** R6 */
-  int R7_tone_a;	/** R7 bit 0 */
-  int R7_tone_b;	/** R7 bit 1 */
-  int R7_tone_c;	/** R7 bit 2 */
-  int R7_noise_a;	/** R7 bit 3 */
-  int R7_noise_b;	/** R7 bit 4 */
-  int R7_noise_c;	/** R7 bit 5 */
-  int vol_a;		/** R8 bits 3-0 */
-  int vol_b;		/** R9 bits 3-0 */
-  int vol_c;		/** R10 bits 3-0 */
-  int env_a;		/** R8 bit 4 */
-  int env_b;		/** R9 bit 4 */
-  int env_c;		/** R10 bit 4 */
-  int env_freq;		/** R11, R12 */
-  int env_style;	/** R13 */
+  int tone_a;           /**< R0, R1 */
+  int tone_b;		/**< R2, R3 */	
+  int tone_c;		/**< R4, R5 */
+  int noise;		/**< R6 */
+  int R7_tone_a;	/**< R7 bit 0 */
+  int R7_tone_b;	/**< R7 bit 1 */
+  int R7_tone_c;	/**< R7 bit 2 */
+  int R7_noise_a;	/**< R7 bit 3 */
+  int R7_noise_b;	/**< R7 bit 4 */
+  int R7_noise_c;	/**< R7 bit 5 */
+  int vol_a;		/**< R8 bits 3-0 */
+  int vol_b;		/**< R9 bits 3-0 */
+  int vol_c;		/**< R10 bits 3-0 */
+  int env_a;		/**< R8 bit 4 */
+  int env_b;		/**< R9 bit 4 */
+  int env_c;		/**< R10 bit 4 */
+  int env_freq;		/**< R11, R12 */
+  int env_style;	/**< R13 */
 }
 ayemu_regdata_t;
 
 
-/** Output sound format */
+/** Output sound format \internal */
 typedef struct
 {
-  int freq;
-  int channels;
-  int bpc;
+  int freq;			/**< sound freq */
+  int channels;			/**< channels (1-mono, 2-stereo) */
+  int bpc;			/**< bits (8 or 16) */
 }
 ayemu_sndfmt_t;
 
+/**
+ * \defgroup libayemu Functions for emulate AY/YM chip
+ */
+/*@{*/
 
-/** Data structure for sound chip emulation */
+/** Data structure for sound chip emulation \internal
+ * 
+ */
 typedef struct
 {
   /* emulator settings */
-  int table[32];		/* table of volumes for chip */
-  ayemu_chip_t type;		/* general chip type (AY or YM) */
-  int ChipFreq;			/* chip frequency */
-  int eq[6];			/* volumes for channels (A left, A right,...) range: -100...100 */
-  ayemu_regdata_t regs;		/* registers data */
-  ayemu_sndfmt_t sndfmt;	/* output sound format */
+  int table[32];		/**< table of volumes for chip */
+  ayemu_chip_t type;		/**< general chip type (\b AYEMU_AY or \b AYEMU_YM) */
+  int ChipFreq;			/**< chip emulator frequency */
+  int eq[6];			/**< volumes for channels.
+				   Array contains 6 elements: 
+				   A left, A right, B left, B right, C left and C right;
+				   range -100...100 */
+  ayemu_regdata_t regs;		/**< parsed registers data */
+  ayemu_sndfmt_t sndfmt;	/**< output sound format */
 
   /* flags */
-  int magic;			/* structure init (init()) flag */
-  int default_chip_flag;
-  int default_stereo_flag;
-  int default_sound_format_flag;
-  int dirty;
+  int magic;			/**< structure initialized flag */
+  int default_chip_flag;	/**< =1 after init, resets in #ayemu_set_chip_type() */
+  int default_stereo_flag;	/**< =1 after init, resets in #ayemu_set_stereo() */
+  int default_sound_format_flag; /**< =1 after init, resets in #ayemu_set_sound_format() */
+  int dirty;			/**< dirty flag. Sets if any emulator properties changed */
 
-  /* emulator valiables */
-  int bit_a, bit_b, bit_c, bit_n; /* current generator state */
-  int cnt_a, cnt_b, cnt_c, cnt_n, cnt_e; /* back counters */
-  int ChipTacts_per_outcount; /* chip's counts per one sound signal count */
-  int Amp_Global;		/* scale factor for amplitude */
-  int vols[6][32];  /* stereo type (channel volumes) and chip table */
-  int EnvNum;		     /* current envilopment number (0...15) */
-  int env_pos;			/* envilop position (0...127) */
-  int Cur_Seed;		       /* random numbers counter */
+  int bit_a;			/**< state of channel A generator */
+  int bit_b;			/**< state of channel B generator */
+  int bit_c;			/**< state of channel C generator */
+  int bit_n;			/**< current generator state */
+  int cnt_a;			/**< back counter of A */
+  int cnt_b;			/**< back counter of B */
+  int cnt_c;			/**< back counter of C */
+  int cnt_n;			/**< back counter of noise generator */
+  int cnt_e;			/**< back counter of envelop generator */
+  int ChipTacts_per_outcount;   /**< chip's counts per one sound signal count */
+  int Amp_Global;		/**< scale factor for amplitude */
+  int vols[6][32];              /**< stereo type (channel volumes) and chip table.
+				   This cache calculated by #table and #eq  */
+  int EnvNum;		        /**< number of current envilopment (0...15) */
+  int env_pos;			/**< current position in envelop (0...127) */
+  int Cur_Seed;		        /**< random numbers counter */
 }
 ayemu_ay_t;
-
-
-/* Interface of library */
 
 EXTERN void
 ayemu_init(ayemu_ay_t *ay);
@@ -123,5 +143,8 @@ ayemu_set_regs (ayemu_ay_t *ay, unsigned char *regs);
 EXTERN void*
 ayemu_gen_sound (ayemu_ay_t *ay, void *buf, size_t bufsize);
 
+/*@}*/
+
 END_C_DECLS
+
 #endif
